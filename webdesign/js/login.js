f15f5096f86ca294b5b6499cac114757dd176854
Marcell Frank <frank.marcell16@gmail.com>
2022-05-22 19:50:29 +0200

    Add files via upload

---
+++ webdesign/js/login.js
@@ -0,0 +1,9 @@
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+})
+
+
+
