a3968d2c2e34d00684005d023241e7e32c9ce032
Marcell Frank <frank.marcell16@gmail.com>
2022-05-23 12:50:13 +0200

    Delete webdesign directory

--- webdesign/js/login.js
+++
@@ -1,9 +0,0 @@
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-
-})
-
-
-
