58e823a9f0d1db76c1d6d022b9c002226d20e7a4
Marcell Frank <frank.marcell16@gmail.com>
2022-05-23 12:33:48 +0200

    test

---
+++ webdesign/js/login.js
@@ -0,0 +1,11 @@
+function loginCreds() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    //User auth -> redirect to main stream
+
+    window.location.href = 'stream.html';
+}
+
+
+
